3a8fd13b27458a375e8c0db2158e7014bb4eabaf
Pörös Dávid <davidporos92@gmail.com>
2019-11-26 21:32:04 +0100

    Create the user interface

---
+++ scripts/Interface.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+export(bool) var muted = false
+
+onready var sound = $UI/MarginContainer/HBoxContainer/Buttons/Sound
+onready var music = $BGM
+onready var lives = $UI/MarginContainer/HBoxContainer/HBoxContainer/Lives
+
+var img_unmuted = preload("res://images/buttons/unmuted.png")
+var img_muted = preload("res://images/buttons/muted.png")
+var lives_0 = preload("res://images/misc/lives_0.png")
+var lives_1 = preload("res://images/misc/lives_1.png")
+var lives_2 = preload("res://images/misc/lives_2.png")
+var lives_3 = preload("res://images/misc/lives_3.png")
+var is_ready = false
+
+func _ready():
+	is_ready = true
+
+func mute():
+	muted = true
+	sound.texture_normal = img_muted
+	music.stream_paused = true
+
+func unmute():
+	muted = false
+	sound.texture_normal = img_unmuted
+	music.stream_paused = false
+
+func _on_Sound_pressed():
+	if muted:
+		unmute()
+	else:
+		mute()
+
+func set_lives(live_count):
+	match live_count:
+		1:
+			lives.texture = lives_1
+		2:
+			lives.texture = lives_2
+		3:
+			lives.texture = lives_3
+		_:
+			lives.texture = lives_0
+
+func _on_Adult_health_changed(live_count):
+	set_lives(live_count)
+
+
+func _on_Back_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal health_changed(live_count)
+
 export(int) var speed = 600
 export(int) var gravity = 50
 export(int) var jump_power = -1000
@@ -7,6 +9,7 @@ export(int) var jump_power = -1000
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
+var lives = 0
 
 func move_left():
 	velocity.x = -speed
@@ -20,6 +23,12 @@ func stay():
 func jump():
 	velocity.y = jump_power
 	$Jump.play()
+
+func add_health():
+	lives += 1
+	emit_signal("health_changed", lives)
+	if lives >= 3:
+		game_over()
 
 func play_animation():
 	if velocity.x < 0:
@@ -38,6 +47,9 @@ func play_animation():
 func win():
 	get_tree().change_scene("res://scenes/Win.tscn")
 
+func game_over():
+	get_tree().change_scene("res://scenes/GameOver.tscn")
+
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		move_right()
@@ -47,6 +59,7 @@ func _physics_process(delta):
 		stay()
 	
 	if Input.is_action_pressed("ui_up") && is_on_floor():
+		add_health()
 		jump()
 	
 	velocity.y += gravity
