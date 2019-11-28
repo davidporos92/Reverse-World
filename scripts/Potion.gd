b2274da48aca36383b75ab7eae07246ed6bf7b6b
David Poros <david.poros@adevinta.com>
2019-11-28 11:09:47 +0100

    Reset potion after 15 sec

--- scripts/Potion.gd
+++ scripts/Potion.gd
@@ -4,4 +4,11 @@ func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body.has_method("shrink"):
 			body.shrink()
-			queue_free()
+			$CollisionShape2D.set_deferred("disabled", true)
+			visible = false
+			$Timer.start()
+
+
+func _on_Timer_timeout():
+	$CollisionShape2D.set_deferred("disabled", false)
+	visible = true
